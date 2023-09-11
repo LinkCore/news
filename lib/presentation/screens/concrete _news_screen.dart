8d882b33eaa6b72b47a86f03c94017b9f3d50422
theEffectOfABrainTumor <90643699+theEffectOfABrainTumor@users.noreply.github.com>
2023-09-11 17:47:37 +0300

    hotfix

--- lib/presentation/screens/concrete _news_screen.dart
+++ lib/presentation/screens/concrete _news_screen.dart
@@ -20,65 +20,64 @@ class ConcreteNewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
+        //elevation: 0,
         title: Text(news.title,
             style: const TextStyle(fontSize: 12),
             overflow: TextOverflow.ellipsis,
             maxLines: 2),
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics()),
-        child: Center(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-            child: Column(
-              children: [
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(35),
-                    child: CachedNetworkImage(imageUrl: news.urlToImage)),
-                const SizedBox(height: 10),
-                ReadingTime(
-                  title: news.title,
-                  content: news.content,
-                  description: news.description,
-                ),
-                const SizedBox(height: 5),
-                Publication(
-                  author: news.author,
-                  publishedAt: news.publishedAt,
-                ),
-                const SizedBox(height: 40),
-                Text(news.content,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w600)),
-                const SizedBox(height: 20),
-                Text(news.description,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w600)),
-                const SizedBox(height: 20),
-                InkWell(
-                    onTap: openOnWeb,
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        width: MediaQuery.of(context).size.width,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: const Color(0xFF191919),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        margin: const EdgeInsets.symmetric(horizontal: 15),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(35),
+                  child: CachedNetworkImage(imageUrl: news.urlToImage)),
+              const SizedBox(height: 10),
+              ReadingTime(
+                title: news.title,
+                content: news.content,
+                description: news.description,
+              ),
+              const SizedBox(height: 5),
+              Publication(
+                author: news.author,
+                publishedAt: news.publishedAt,
+              ),
+              const SizedBox(height: 40),
+              Text(news.content,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600)),
+              const SizedBox(height: 20),
+              Text(news.description,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600)),
+              const SizedBox(height: 20),
+              InkWell(
+                  onTap: openOnWeb,
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      width: MediaQuery.of(context).size.width,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color(0xFF191919),
+                      ),
+                      child: const Text(
+                        _S.openOnWeb,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
                         ),
-                        child: const Text(
-                          _S.openOnWeb,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ))),
-              ],
-            ),
+                      ))),
+            ],
           ),
         ),
       ),
