444d247d9cbec1b0f5e34759a359393ff9150367
theEffectOfABrainTumor <90643699+theEffectOfABrainTumor@users.noreply.github.com>
2023-09-12 11:22:04 +0300

    reading time fix

--- lib/common/app_utilities.dart
+++ lib/common/app_utilities.dart
@@ -2,7 +2,7 @@ import 'package:news/generated/l10n.dart';
 
 abstract class AppUtilities {
   static String readingTime(String text) {
-    const int readingSpeed = 70;
+    const int readingSpeed = 50;
     final int timeInMinutes = text.split(' ').length ~/ readingSpeed;
     return S.current.pageNotificationsCount(timeInMinutes);
   }
